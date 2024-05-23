1455a6a7d1387b878721732d17e9ced396c32235
anh.do <tuananh9h@gmail.com>
2024-05-23 10:06:16 +0700

    useEffect With DOM Events F8

--- src/hooks/F8/useEffect/Content.tsx
+++ src/hooks/F8/useEffect/Content.tsx
@@ -1,12 +1,4 @@
 import { useEffect, useState } from "react";
-import {
-  PostType,
-  CommentType,
-  AlbumType,
-  PhotoType,
-  TodoType,
-  UserType,
-} from "./TypeArray";
 
 // 1. useEffect(callback)
 // - Gọi callback mỗi khi component re-render
@@ -14,17 +6,23 @@ import {
 // 2. useEffect(callback, [])
 // - Chỉ gọi callback 1 lần sau khi component mounted
 // 3. useEffect(callback, [deps])
+// - Gọi callback mỗi khi deps thay đổi
 
 // ----------
 // 1. Callback luôn được gọi sau khi component mounted
+// 2. Cleanup function luôn được gọi trước khi component unmounted
 
 const tabs = ["posts", "comments", "albums", "photos", "todos", "users"];
-const T = [PostType, CommentType, AlbumType, PhotoType, TodoType, UserType];
 
 function Content() {
   const [title, setTitle] = useState("");
-  const [type, setType] = useState("type");
-  const [posts, setPosts] = useState<T[][]>([]);
+  const [data, setData] = useState([]);
+  const [type, setType] = useState("posts");
+
+  const [showGoToTop, setShowGoToTop] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  console.log(type);
 
   useEffect(() => {
     document.title = title;
@@ -38,23 +36,81 @@ function Content() {
   //   }, []);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    console.log("Type changed");
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((response) => response.json())
-      .then((posts) => setPosts(posts));
+      .then((data) => setData(data));
+  }, [type]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY);
+      if (window.scrollY >= 200) {
+        setShowGoToTop(true);
+        console.log("set state");
+      } else {
+        setShowGoToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    console.log("addEventListener");
+
+    // Cleanup function
+    return () => {
+      console.log("Unmounting...");
+      window.removeEventListener("scroll", handleScroll);
+      console.log("removeEventListener");
+    };
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      console.log(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function
+    return () => {
+      console.log("Unmounting...");
+      window.removeEventListener("resize", handleResize);
+      console.log("removeEventListener");
+    };
   }, []);
+
+  console.log("re-render");
 
   return (
     <div style={{ marginTop: "24px" }}>
+      <h1>{width}</h1>
       {tabs.map((tab) => (
-        <button key={tab} onClick={() => setType(tab)}>
+        <button
+          key={tab}
+          style={type === tab ? { color: "#646cff" } : {}}
+          onClick={() => setType(tab)}
+        >
           {tab}
         </button>
       ))}
       <input onChange={(e) => setTitle(e.target.value)} value={title} />
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+        {data.map((detail) => (
+          <li key={detail.id}>{detail.title || detail.name}</li>
         ))}
+        {showGoToTop && (
+          <button
+            style={{
+              position: "fixed",
+              right: "20px",
+              bottom: "20px",
+            }}
+            onClick={() => window.scrollTo(0, 0)}
+          >
+            Go to top
+          </button>
+        )}
       </ul>
     </div>
   );
